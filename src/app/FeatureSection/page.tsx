964bd26b576f66ee506b455bce49c120061f99ac
Karuiru Jackson <jkjackaruiru641@gmail.com>
2024-11-20 17:16:29 +0300

    Make the Feature section responsive in all devices

--- src/app/FeatureSection/page.tsx
+++ src/app/FeatureSection/page.tsx
@@ -12,16 +12,16 @@ export default function FeatureSection() {
   return (
     <div className="h-screen py-16">
       <div className="flex flex-col gap-12 px-4">
-        <section className="flex flex-col items-center justify-center gap-4">
-          <h2 className="text-[#111827] text-3xl leading-8 tracking-tight font-extrabold">Engage with your audience</h2>
-          <p className="text-xl leading-7 font-normal text-[#6B7280]">With Along With, you can invite your community to join you in real-time, cooking along with you and sharing the joy of culinary creation.</p>
+        <section className="flex flex-col md:text-center items-center justify-center gap-4">
+          <h2 className="text-[#111827] text-3xl md:text-4xl md:leading-10 leading-8 tracking-tight font-extrabold">Engage with your audience</h2>
+          <p className="text-xl leading-7 font-normal md:w-2/3 text-[#6B7280]">With Along With, you can invite your community to join you in real-time, cooking along with you and sharing the joy of culinary creation.</p>
         </section>
-        <section className="flex flex-col gap-10">
-          <div className="flex flex-col gap-3">
-            <h2 className="text-[#111827] text-2xl leading-8 font-extrabold tracking-tight">Personalised & Dynamic Recipes</h2>
-            <p className="text-lg leading-7 font-normal text-[#6B7280]">Build your recipes in your own style to allow your fans to follow along with your videos and create good meals together. We provide a dynamic ingredients list that caters to how many portions thy intend to cook.</p>
-          </div>
-          <div className="flex flex-col gap-10">
+        <section className="flex flex-col md:flex-row gap-10">
+          <div className="flex flex-col md:gap-6 gap-10">
+            <div className="flex flex-col gap-3">
+              <h2 className="text-[#111827] text-2xl md:text-3xl md:leading-9 leading-8 font-extrabold tracking-tight">Personalised & Dynamic Recipes</h2>
+              <p className="text-lg leading-7 font-normal text-[#6B7280]">Build your recipes in your own style to allow your fans to follow along with your videos and create good meals together. We provide a dynamic ingredients list that caters to how many portions thy intend to cook.</p>
+            </div>
             <div className="flex flex-row gap-4 items-start">
               <Image src={BrandPen} alt="Brand-icon-pen" className="" />
               <div className="flex flex-col gap-2">
@@ -45,18 +45,18 @@ export default function FeatureSection() {
             </div>
           </div>
           <div className="relative h-[500px] w-full">
-            <div className="absolute top-16">
+            <div className="absolute md:top-10 left-20 ">
               <Image src={DotPattern} alt="Dot-Pattern" />
             </div>
-            <Image src={IngredientsRecipe} alt="Ingredient-Recipe" className="absolute left-11" />
+            <Image src={IngredientsRecipe} alt="Ingredient-Recipe" className="absolute md:top-10 left-11" />
           </div>
         </section>
-        <section className="flex flex-col gap-10">
-          <div className="flex flex-col gap-3">
-            <h2 className="text-[#111827] text-2xl leading-8 font-extrabold tracking-tight">One-of-a-kind Shopping Experience</h2>
-            <p className="text-lg leading-7 font-normal text-[#6B7280]">Our goal is to make cooking enjoyable and stress-free for your audience. We offer a user-friendly experience that helps your audience obtain all the necessary ingredients to prepare delicious meals from scratch.</p>
-          </div>
+        <section className="flex flex-col md:flex-row-reverse md:gap-8 gap-10">
           <div className="flex flex-col gap-10">
+            <div className="flex flex-col gap-3">
+              <h2 className="text-[#111827] text-2xl md:text-3xl md:leading-9 leading-8 font-extrabold tracking-tight">One-of-a-kind Shopping Experience</h2>
+              <p className="text-lg leading-7 font-normal text-[#6B7280]">Our goal is to make cooking enjoyable and stress-free for your audience. We offer a user-friendly experience that helps your audience obtain all the necessary ingredients to prepare delicious meals from scratch.</p>
+            </div>
             <div className="flex flex-row gap-4 items-start">
               <Image src={BrandLedger} alt="Brand-icon-pen" className="" />
               <div className="flex flex-col gap-2">
